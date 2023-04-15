72285e7d7e8a648304ccd46eb81d01fb88c41bd5
segundo <90773948+szgundo@users.noreply.github.com>
2023-04-15 09:37:48 -0300

    01

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>hello world</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+                    Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+                    Learn React
         </a>
       </header>
     </div>
